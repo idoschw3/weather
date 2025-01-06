17a63c1b1541600eeccd8c94871747212419ef45
idoschw3 <ido.schwartz.il@gmail.com>
2025-01-06 12:04:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ st.title("Weather App")
 name = st.text_input("Enter your name:")
 if name:
     st.write(f"Hello {name}, welcome to the weather app")
+    st.write(f"your local time: {get_system_local_time()}.")
     city = st.text_input("At which city you wish to check the current weather?")
     if city:
-        st.write(f"your local time: {get_system_local_time()}.")
         st.write(f"{city} local time: {get_city_local_time(city)}.")
         if st.button("show weather data"):
             key = '63e9898f15bf448cbe4130843250501'
