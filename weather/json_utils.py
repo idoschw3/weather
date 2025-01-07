6226145dd3e32b76f19224e1151b5ac764cc1282
idoschw3 <ido.schwartz.il@gmail.com>
2025-01-07 10:24:31 +0200

    create json functions

---
+++ weather/json_utils.py
@@ -0,0 +1,31 @@
+import json
+
+def load_json(file_path):
+    """
+    Load JSON data from a file.
+
+    Args:
+        file_path (str or Path): Path to the JSON file.
+
+    Returns:
+        dict or list: Parsed JSON data.
+
+    """
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    return data
+
+def append_to_json(file_path, new_data):
+    """
+        Append new data to the JSON file.
+
+        Args:
+            file_path (str): The path to the JSON file.
+            new_data (dict): The new data to append (key-value pairs).
+    """
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+        data.update(new_data)
+        return data
